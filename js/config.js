e5baaddd12f3a22454e56f4f43b117caac96ba57
unknown <sistemasovopacific@ovopacific.local>
2026-06-30 15:58:02 -0500

    Update API URL for creator column implementation

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.appConfig = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzjii5dn5EVz3JQm64RHa2Ye4HWBRWUTeGsYoJbltZuRjlpycMpnfB4k1D9v2dhKC7cpw/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyAndil3eZpSwTZ6EIvJk8q12b1_cAdBwehh14GtgI0aOLn14AYW9gxvXRWDG93rHHdgw/exec'
 };
 
