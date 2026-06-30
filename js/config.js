603468bc8b03dd9e44687cc5b68f17c161aef796
unknown <sistemasovopacific@ovopacific.local>
2026-06-30 15:28:24 -0500

    Update API URL with CORS fix

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.appConfig = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxHOpFAIlauP2XqZzRSMDo_sd1eT3h0myAVVrzk68OEuop8g538uyD51m_02RWzqW8mPQ/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzjii5dn5EVz3JQm64RHa2Ye4HWBRWUTeGsYoJbltZuRjlpycMpnfB4k1D9v2dhKC7cpw/exec'
 };
 
